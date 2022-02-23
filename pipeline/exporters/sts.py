8b414b41daef5e8bf65028b124278f8d83e20eff
charles.viegas <charles.viegas@gmail.com>
2022-02-23 14:53:23 -0400

    Renaming files and merge pesquisas prontas

--- pipeline/exporters/sts.py
+++ pipeline/exporters/sts.py
@@ -5,68 +5,69 @@ from sklearn.model_selection import train_test_split
 from pipeline.utils import WorkProgress, DatasetManager, PathUtil, correct_spelling
 
 
-def split_train_test(dataset):
-    train_samples, test_samples = train_test_split(dataset, train_size=0.80, test_size=0.20, random_state=103,
-                                                   shuffle=True)
-    return train_samples, test_samples
-
-
 class StsExporter:
+    ID_FIELD = 'id'
+
     TEXT_FIELD = 'ementa'
 
-    # SHOULD_SPLIT = False
-    SHOULD_SPLIT = True
+    GROUP_FIELDS = [['assunto'], ['area', 'tema', 'discussao']]
 
-    ID_FIELD = 'titulo'
-    # ID_FIELD = 'acordao_id'
-
-    GROUP_FIELDS = ['area', 'tema', 'discussao']
-    # GROUP_FIELDS = ['assunto']
-
-    SOURCE_FILENAME = 'pesquisas-prontas-stf.csv'
-    # SOURCE_FILENAME = 'annotated-queries.csv'
+    SOURCE_FILENAMES = ['pesquisas-prontas-tjms.csv', 'pesquisas-prontas-stf.csv']
 
     HEADER = {'assunto': [], 'id1': [], 'ementa1': [], 'id2': [], 'ementa2': [], 'similarity': []}
 
     def __init__(self):
         self.work_progress = WorkProgress()
         self.dataset_manager = DatasetManager()
-        self.annotated_dataset = None
         self.sts_dataset = pd.DataFrame(self.HEADER)
 
     def execute(self):
         self.work_progress.show('Preparing dataset for STS')
-        self._read_annotated_dataset()
-        self._match_similar_sentences()
-        self._match_unsimilar_sentences()
-        if self.SHOULD_SPLIT:
-            train_dataset, dev_dataset = self._split_dataset()
-            self._save_sts_dataset(train_dataset, 'test')
-            self._save_sts_dataset(dev_dataset, 'dev')
-        else:
-            self._save_sts_dataset(self.sts_dataset, 'eval')
+        for index, filename in enumerate(self.SOURCE_FILENAMES):
+            dataset = self._read_annotated_dataset(filename)
+            groups = self._get_groups_from_dataset(dataset, index)
+            similar_dataset = self._match_similar_sentences(groups)
+            unsimilar_dataset = self._match_unsimilar_sentences(groups)
+            self._accumulate_dataset(similar_dataset)
+            self._accumulate_dataset(unsimilar_dataset)
+
+        self._print_summary(self.sts_dataset, 'total')
+        train_dataset, dev_dataset, test_dataset = self._split_train_dataset()
+        self._print_summary(train_dataset, 'train')
+        self._save_sts_dataset(train_dataset, 'train')
+
+        self._print_summary(dev_dataset, 'dev')
+        self._save_sts_dataset(dev_dataset, 'dev')
+
+        self._print_summary(test_dataset, 'test')
+        self._save_sts_dataset(test_dataset, 'test')
         self.work_progress.show('STS dataset has finished!')
 
-    def _read_annotated_dataset(self):
+    def _read_annotated_dataset(self, filename):
         self.work_progress.show('Reading annotated dataset')
-        annotated_filepath = PathUtil.build_path('resources', self.SOURCE_FILENAME)
-        self.annotated_dataset = self.dataset_manager.from_csv(annotated_filepath)
+        annotated_filepath = PathUtil.build_path('resources', filename)
+        return self.dataset_manager.from_csv(annotated_filepath)
 
-    def _match_similar_sentences(self):
+    def _get_groups_from_dataset(self, annotated_dataset, index):
+        group_fields = self.GROUP_FIELDS[index]
+        return annotated_dataset.groupby(group_fields)
+
+    def _match_similar_sentences(self, groups):
         self.work_progress.show('Match similar sentences')
-        groups = self.annotated_dataset.groupby(self.GROUP_FIELDS)
+        sts_dataset = pd.DataFrame(self.HEADER)
         for group_name, group in groups:
             self.work_progress.show(f'Processing group {group_name} with {len(group)} itens')
             pairs = list(pairwise(group.index))
             for pair in pairs:
                 sentence1 = group.loc[pair[0]]
                 sentence2 = group.loc[pair[1]]
-                item = self._create_item(sentence1, sentence2, similarity=1)
-                self.sts_dataset = self.sts_dataset.append(item, ignore_index=True)
+                item = self._create_item(group_name, sentence1, sentence2, similarity=1)
+                sts_dataset = sts_dataset.append(item, ignore_index=True)
+        return sts_dataset
 
-    def _match_unsimilar_sentences(self):
+    def _match_unsimilar_sentences(self, groups):
         self.work_progress.show('Match unsimilar sentences')
-        groups = self.annotated_dataset.groupby(self.GROUP_FIELDS)
+        sts_dataset = pd.DataFrame(self.HEADER)
         group_pairs = list(pairwise(groups))
         for group_pair in group_pairs:
             group1_name = group_pair[0][0]
@@ -78,20 +79,22 @@ class StsExporter:
             for pair in sentence_pairs:
                 sentence1 = group1_data.loc[pair[0]]
                 sentence2 = group2_data.loc[pair[1]]
-                item = self._create_item(sentence1, sentence2, similarity=0)
-                self.sts_dataset = self.sts_dataset.append(item, ignore_index=True)
+                item = self._create_item(group1_name, sentence1, sentence2, similarity=0)
+                sts_dataset = sts_dataset.append(item, ignore_index=True)
             sentence_pairs = list(zip_offset(group1_data.index, group2_data.index, offsets=(0, 1), longest=False))
             for pair in sentence_pairs:
                 sentence1 = group1_data.loc[pair[0]]
                 sentence2 = group2_data.loc[pair[1]]
-                item = self._create_item(sentence1, sentence2, similarity=0)
-                self.sts_dataset = self.sts_dataset.append(item, ignore_index=True)
+                item = self._create_item(group2_name, sentence1, sentence2, similarity=0)
+                sts_dataset = sts_dataset.append(item, ignore_index=True)
+        return sts_dataset
 
-    def _create_item(self, sentence1, sentence2, similarity):
-        key_group = sentence1[self.GROUP_FIELDS]
-        key_group = '/'.join(key_group)
+    def _create_item(self, keygroup, sentence1, sentence2, similarity):
+        assunto = keygroup
+        if type(keygroup) == tuple:
+            assunto = ' '.join(keygroup)
         return {
-            'assunto': key_group,
+            'assunto': assunto,
             'id1': sentence1[self.ID_FIELD],
             'ementa1': correct_spelling(sentence1[self.TEXT_FIELD]),
             'id2': sentence2[self.ID_FIELD],
@@ -99,10 +102,28 @@ class StsExporter:
             'similarity': similarity
         }
 
-    def _split_dataset(self):
-        return split_train_test(self.sts_dataset)
+    def _accumulate_dataset(self, dataset):
+        self.sts_dataset = self.sts_dataset.append(dataset, ignore_index=True)
+
+    def _split_train_dataset(self):
+        train_samples, alltest_samples = train_test_split(self.sts_dataset,
+                                                          train_size=0.70,
+                                                          test_size=0.30,
+                                                          random_state=103,
+                                                          shuffle=True)
+        eval_samples, test_samples = train_test_split(alltest_samples,
+                                                      train_size=0.25,
+                                                      test_size=0.75,
+                                                      random_state=99,
+                                                      shuffle=True)
+        return train_samples, eval_samples, test_samples
+
+    def _print_summary(self, dataset, name):
+        self.work_progress.show(f'')
+        self.work_progress.show(f'Dataset for {name}')
+        self.work_progress.show(f'Rows {len(dataset)}')
 
     def _save_sts_dataset(self, dataset, name):
-        self.work_progress.show('Saving STS dataset')
         train_filepath = PathUtil.build_path('output', 'sts', f'{name}.csv')
+        self.work_progress.show(f'Saving STS dataset {train_filepath}')
         self.dataset_manager.to_csv(dataset, train_filepath)
